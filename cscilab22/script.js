d6dc71242990095f9ec9a66650f335bb2af00b17
juliebdick <juliebdick@gmail.com>
2021-09-11 18:25:15 -0500

    blank image and duck url 3

--- cscilab22/script.js
+++ cscilab22/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $('#clicker').click(function() {
     $.ajax({
-      //dataType: "json",
-      url: "https://random-d.uk/api",
+      dataType: "json",
+      url: "https://random-d.uk/api?format=json",
       success: function(results) {
         console.log(results["url"]);
         if (results["url"].endsWith(".gif")) {
-          $('#dog').attr("src", "images/blank.png");
+          $('#dog').attr("src", "blank.png");
         } else {
           $('#dog').attr("src", results["url"]);
         }
