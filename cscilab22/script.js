327f0c00ad5651e58ffa4f312ca118e270b838ae
juliebdick <juliebdick@gmail.com>
2021-09-11 18:18:27 -0500

    Duck url 2

--- cscilab22/script.js
+++ cscilab22/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('#clicker').click(function() {
     $.ajax({
-      dataType: "json",
-      url: "https://random-d.uk/api?format=json",
+      //dataType: "json",
+      url: "https://random-d.uk/api",
       success: function(results) {
         console.log(results["url"]);
-        if (results["url"].endsWith(".mp4")) {
+        if (results["url"].endsWith(".gif")) {
           $('#dog').attr("src", "images/blank.png");
         } else {
           $('#dog').attr("src", results["url"]);
