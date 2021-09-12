ddab03b740e18f69cf0d61cf91ea0012b7474c24
juliebdick <juliebdick@gmail.com>
2021-09-11 20:32:48 -0500

    random images in concole working

--- cscilab22/script.js
+++ cscilab22/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       url: "https://api.thecatapi.com/v1/images/search?format=json",
       success: function(results) {
         console.log(results[0]);
-        $('#dog').attr("src", results[0].get("url"));
+        $('#dog').attr("src", results["url]");
         //if (results["url"].endsWith(".mp4")) {
           //$('#dog').attr("src", "blank.png");
         //} else {
