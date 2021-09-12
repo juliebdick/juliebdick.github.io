d8f21bd3768e8452cac98a65ed38ae61ff450ff5
juliebdick <juliebdick@gmail.com>
2021-09-11 20:50:10 -0500

    commas, not semi colons

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   $('#clicker').click(function() {
     $.ajax({
-      dataType: "json"
-      url: "https://api.thecatapi.com/v1/images/search$format=json"
+      dataType: "json",
+      url: "https://api.thecatapi.com/v1/images/search$format=json",
       success: function(results) {
         console.log(results[0]);
         $('#cat').attr("src", results[0]["url"]);
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
       }
     })
   })
