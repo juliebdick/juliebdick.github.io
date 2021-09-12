03c1abbd5bca5ab3b5f0ee9f72b8cb8c1aa9011a
juliebdick <juliebdick@gmail.com>
2021-09-11 20:47:27 -0500

    cat api json is an array

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   $('#clicker').click(function() {
     $.ajax({
-      //dataType: "json"
-      url: "https://api.thecatapi.com/v1/images/search"
+      dataType: "json"
+      url: "https://api.thecatapi.com/v1/images/search$format=json"
       success: function(results) {
         console.log(results[0]);
+        $('#cat').attr("src", results[0]["url"]);
       }
     })
   })
