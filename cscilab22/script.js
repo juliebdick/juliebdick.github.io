cdd66d055982f9cf60210c16ce490cfbfd1d8775
juliebdick <juliebdick@gmail.com>
2021-09-11 20:23:09 -0500

    no get

--- cscilab22/script.js
+++ cscilab22/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
       dataType: "json",
       url: "https://api.thecatapi.com/v1/images/search?format=json",
       success: function(results) {
-        console.log(results[0].get("url"));
+        console.log(results[0]);
         $('#dog').attr("src", results[0].get("url"));
         //if (results["url"].endsWith(".mp4")) {
           //$('#dog').attr("src", "blank.png");
