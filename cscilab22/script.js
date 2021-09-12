d8ba8527f5108ee27c071781ff274b7a26a09c03
juliebdick <juliebdick@gmail.com>
2021-09-11 19:22:51 -0500

    cat json

--- cscilab22/script.js
+++ cscilab22/script.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
   $('#clicker').click(function() {
     $.ajax({
       dataType: "json",
-      url: "https://random-d.uk/api?format=json",
+      url: "https://api.thecatapi.com/v1/images/search?format=json",
       success: function(results) {
         console.log(results["url"]);
-        if (results["url"].endsWith(".gif")) {
+        if (results["url"].endsWith(".mp4")) {
           $('#dog').attr("src", "blank.png");
         } else {
-          $('#dog').attr("src", results[0]);
+          $('#dog').attr("src", results["url"]);
         }
       },
       error: function(xhr,status,error) {
